8cc48c5ec8e10e3c4769181a4497c120c9b9bf42
marcelbernic <marcel.bernic.1@ulaval.ca>
2018-03-11 21:38:40 -0400

    removed empty data object

--- src/app.js
+++ src/app.js
@@ -1,38 +1,16 @@
 import './assets/styles/styles.scss';
 
-var dataSet = {
-    rank: 1,
-    name: "Team",
-    score: 0,
-    penalty: 0,
-    user_stories: [
-        { "number": 1, "status": "uncompleted" },
-        { "number": 2, "status": "uncompleted" },
-        { "number": 3, "status": "uncompleted" },
-        { "number": 4, "status": "uncompleted" },
-        { "number": 5, "status": "uncompleted" },
-        { "number": 6, "status": "uncompleted" },
-        { "number": 7, "status": "uncompleted" },
-        { "number": 8, "status": "uncompleted" },
-        { "number": 9, "status": "uncompleted" },
-        { "number": 10, "status": "uncompleted" },
-    ],
-    build: "failed",
-    test: "failed"
-};
-
 _.templateSettings.variable = "team";
 var template = _.template(
     $("script.tmpl-TeamStats").html()
 );
 
-for (var i = 1; i < 30; i++) {
-    if (i < 16) {
-        $("#left-panel").append(template(dataSet));
+for (var i = 1; i < 28; i++) {
+    if (i < 14) {
+        $('#left-panel').append(`<div id="${i}"></div>`);
     } else {
-        $("#right-panel").append(template(dataSet));
+        $('#right-panel').append(`<div id="${i}"></div>`);
     }
-    document.getElementById("rank").setAttribute("id", i);
 }
 
 setInterval(function(){
@@ -40,12 +18,12 @@ setInterval(function(){
         .then(function (response) {
             for (var i = 0; i < 2; i++) {
                 var teamRank = JSON.parse(response.request.response).results[i].rank;
-                $('#'+teamRank).replaceWith(template(JSON.parse(response.request.response).results[i]));
+                $(`#${teamRank}`).replaceWith(template(JSON.parse(response.request.response).results[i]));
             }
         })
         .catch(function (error) {
             console.log(error);
         });
-}, 1000);
+}, 5000);
 
 
